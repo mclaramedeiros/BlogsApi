da8bb7b80bf7680902c876c09c5f9df313f951f7
Maria Clara <claramedeiroswon@gmail.com>
2022-07-13 20:04:22 -0300

    feat: add models

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,31 +1,31 @@
-// 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   const Users = sequelize.define('User', {
-//     id: {
-//       allowNull: false,
-//       autoIncrement: true,
-//       primaryKey: true,
-//       type: DataTypes.INTEGER
-//     },
-//     displayName: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     image: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//   }, 
-//   {
-//     tableName: 'User'
-//   });
-//   return Users;
-// };
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, 
+  {
+    tableName: 'User'
+  });
+  return Users;
+};
